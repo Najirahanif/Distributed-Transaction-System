db5da9b5718a31492c711c905dd6b77402b1b7d9
Najira Banu <najirabanu@lemonpay.tech>
2026-07-22 17:09:14 +0530

    HPA load testing

--- order-service/src/app.js
+++ order-service/src/app.js
@@ -31,6 +31,18 @@ app.get("/", (req, res) => {
         message: "Order Service is running..."
     });
 });
+// In order to check the Horizontal Pod Autoscaling and its usecase(HPA)
+app.get("/load", (req, res) => {
+    const end = Date.now() + 1000;
+
+    while (Date.now() < end) {
+        Math.sqrt(Math.random() * 1000000);
+    }
+
+    res.json({
+        message: "CPU load generated"
+    });
+});
 
 // Routes
 app.use("/orders", orderRoutes);
